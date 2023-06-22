008cc3d5c2f8a45f205f58f446f872b53c70f6f8
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-22 17:31:58 +0200

    Create JsonValue.java

---
+++ src/net/fexcraft/app/json/JsonValue.java
@@ -0,0 +1,67 @@
+package net.fexcraft.app.json;
+
+/**
+ * 
+ * @author Ferdinand Calo' (FEX___96)
+ *
+ */
+public class JsonValue<V> implements FJson {
+	
+	public V value;
+	
+	public JsonValue(){}
+	
+	public JsonValue(V value){
+		this.value = value;
+	}
+	
+	public <VALUE> VALUE value(){
+		return (VALUE)value;
+	}
+	
+	public void value(V newval){
+		this.value = newval;
+	}
+
+	@Override
+	public boolean isObject(){
+		return true;
+	}
+	
+	@Override
+	public String toString(){
+		return JsonHandler.toString(this);
+	}
+
+	public float float_value(){
+		if(value instanceof Number == false) return 0;
+		return ((Number)value).floatValue();
+	}
+
+	public int integer_value(){
+		if(value instanceof Number == false) return 0;
+		return ((Number)value).intValue();
+	}
+
+	public long long_value(){
+		if(value instanceof Number == false) return 0;
+		return ((Number)value).longValue();
+	}
+
+	public String string_value(){
+		return value == null ? null : value.toString();
+	}
+
+	public boolean isNumber(){
+		return value instanceof Number;
+	}
+
+	public boolean bool(){
+		return (Boolean)value;
+	}
+
+	public JsonValue<V> copy(){
+		return new JsonValue<V>(value);
+	}
+
+}
